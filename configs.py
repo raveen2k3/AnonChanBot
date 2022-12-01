ad186c0b9bb57cea9b0bed4b73705994c6e24ea0
RAVEEN KUMAR R <63842813+raveen2k3@users.noreply.github.com>
2022-12-01 16:34:00 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,11 @@
+import os
+import os
+from anonfile import AnonFile
+
+BOT_TOKEN = " "
+API_ID = int(" ")
+API_HASH = " "
+ANON_TOKEN = AnonFile(token=" ")
+
+
+#register here to get token https://anonfiles.com/register
